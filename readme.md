ea326b165f625b6b2607ac319d22f73708432373
Raman Saran Verma <raman.verma@redbus.in>
2016-06-19 10:36:07 +0530

    final

---
+++ readme.md
@@ -0,0 +1,8 @@
+We have made a application that makes life easy. 
+This application can suggest trip plans which fits best under your budeget. 
+Passengers have facility that they can enter destination and source including with the 
+budget amount and then this application will assist with different options 
+for bus, flight booking and hotel booking.
+
+
+Please use <your key> for getTrain API
--- routes/apiController/getRailFare.js
+++ routes/apiController/getRailFare.js
@@ -1,7 +1,7 @@
 var unirest = require('unirest');
 
 module.exports = function(req, res, next) {
-    var url = "http://api.railwayapi.com/fare/train/"+ req.params.tno +"/source/"+ req.params.src +"/dest/"+ req.params.dst +"/age/18/quota/GN/doj/"+ req.params.doj +"/apikey/cqjcc6945/";
+    var url = "http://api.railwayapi.com/fare/train/"+ req.params.tno +"/source/"+ req.params.src +"/dest/"+ req.params.dst +"/age/18/quota/GN/doj/"+ req.params.doj +"/apikey/<your key>";
     console.log(url);
     var dd={ "train": { "name": "CHENNAI EXPRESS", "number": "12610" }, "response_code": 200, "quota": { "code": "GN", "name": "GENERAL QUOTA" }, "fare": [{ "code": "CC", "name": "AC CHAIR CAR", "fare": "540" }, { "code": "2S", "name": "SECOND SEATING", "fare": "150" }], "from": { "code": "SBC", "name": "BANGALORE CY JN" }, "to": { "code": "MAS", "name": "CHENNAI CENTRAL" }, "failure_rate": 14.634146341463413 };
     res.send(dd);
